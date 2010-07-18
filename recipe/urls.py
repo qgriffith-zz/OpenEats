bd2e5ad2d3ce4febaaa04e5e8a0f6b24dcdcc631
Quenten Griffith <qgriffith@gmail.com>
2010-07-18 16:34:15 -0400

    added a print view for recipes

--- recipe/urls.py
+++ recipe/urls.py
@@ -6,6 +6,7 @@ recipe_info={
     'queryset': Recipe.objects.all(),
     "template_object_name": 'recipe',
 }
+
 recipe_list={
     'queryset': Recipe.objects.order_by('pub_date', 'title')[:10],
     "template_object_name": 'recipe',
@@ -15,6 +16,7 @@ recipe_list={
 
 urlpatterns = patterns('',
     (r'^new/$', 'recipe.views.recipe'),
+    (r'^print/(?P<slug>[-\w]+)/$', list_detail.object_detail, {'queryset': Recipe.objects.all(), "template_object_name": 'recipe',"template_name": 'recipe/recipe_print.html',}),
     (r'^(?P<slug>[-\w]+)/$', list_detail.object_detail, recipe_info),
     (r'^', list_detail.object_list, recipe_list),
    )
