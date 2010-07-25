9fd51e3835a2636a20a34ad8fb85f4cc7821f99f
Quenten Griffith <qgriffith@gmail.com>
2010-07-25 16:43:20 -0400

    moved the java script for the autocomplete to its own js file instead of being in the recipe_form

---
+++ site_media/js/oe.js
@@ -0,0 +1,24 @@
+/* javascript file for OpenEats2*/
+
+/* this function is for the django-dynamic-formset which allows a new ingredient row to be added on the recipe form*/
+$(function() {
+    $('#ing-5').formset({
+        prefix: '{{ formset.prefix }}',
+        added: function(row){
+            var txt = row.find('#ing-field input');
+            txt.unbind();
+            makeAutoComplete(); /* call the makeAutoComplete function to assign the jquery ui autocomplete to the new row added*/
+        },
+        extraClasses: ['row1', 'row2']
+    });
+})
+
+
+function makeAutoComplete() {
+    $(function() {
+        $("#ing-field input").autocomplete({
+            source: '/ingredient/auto/'
+        });
+    })
+    };
+makeAutoComplete()
