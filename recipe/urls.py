c5e213ae49570917bea61a70d2e51d616fefdfce
Quenten Griffith <qgriffith@gmail.com>
2010-08-09 19:22:59 -0400

    added a new cook view to allow people to be able to look at recipes from long distances

--- recipe/urls.py
+++ recipe/urls.py
@@ -17,6 +17,7 @@ recipe_list={
 urlpatterns = patterns('',
     (r'^new/$', 'recipe.views.recipe'),
     (r'^print/(?P<slug>[-\w]+)/$', list_detail.object_detail, {'queryset': Recipe.objects.all(), "template_object_name": 'recipe',"template_name": 'recipe/recipe_print.html',}),
+    (r'^cook/(?P<slug>[-\w]+)/$', list_detail.object_detail, {'queryset': Recipe.objects.all(), "template_object_name": 'recipe',"template_name": 'recipe/recipe_cook.html',}),
     (r'^(?P<slug>[-\w]+)/$', list_detail.object_detail, recipe_info),
     (r'^', list_detail.object_list, recipe_list),
    )
