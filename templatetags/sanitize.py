c654e394a8b48ecff438c389145da52afb3731d5
Quenten Griffith <qgriffith@gmail.com>
2010-08-15 15:42:03 -0400

    added an html sanitze filter using html5lib now the recipe info and directions will be santized prior to display

---
+++ templatetags/sanitize.py
@@ -0,0 +1,19 @@
+import html5lib
+from html5lib import treebuilders, treewalkers, serializer, sanitizer
+from django import template
+from django.template.defaultfilters import stringfilter
+register = template.Library()
+
+@stringfilter
+@register.filter(name='sanitize_html')
+def sanitize_html(value):
+    p = html5lib.HTMLParser(tokenizer=sanitizer.HTMLSanitizer, tree=treebuilders.getTreeBuilder("dom"))
+    dom_tree = p.parseFragment(value)
+
+    walker = treewalkers.getTreeWalker("dom")
+
+    stream = walker(dom_tree)
+
+    s = serializer.htmlserializer.HTMLSerializer(omit_optional_tags=False)
+    return "".join(s.serialize(stream))
+    
