ec08b87a30900dc379bf0137229c719caf5fc030
Quenten Griffith <qgriffith@gmail.com>
2010-08-14 21:46:12 -0400

    added a new filter to count recipes based on shared or private

---
+++ templatetags/recipe_count.py
@@ -0,0 +1,14 @@
+from django import template
+from django.contrib.auth.models import User
+register = template.Library()
+
+@register.filter(name='recipeCount')
+def recipeCount(value, arg):
+    '''takes the value, which is the users name and counts the number of private or shared recipes'''
+    user = User.objects.get(username=value)
+    if arg == 'shared':
+        return user.recipe_set.filter(shared=0).count()
+    else:
+        return user.recipe_set.filter(shared=1).count()
+
+    
