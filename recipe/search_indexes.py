75affe72ec2349a92691d5f457cda2811b1be7c7
Quenten Griffith <qgriffith@gmail.com>
2010-09-06 15:31:14 -0400

    added a very basic search page

--- recipe/search_indexes.py
+++ recipe/search_indexes.py
@@ -6,10 +6,11 @@ from models import Recipe
 class RecipeIndex(SearchIndex):
     text = CharField(document=True, use_template=True)
     author = CharField(model_attr='author')
+    course = CharField(model_attr='course')
+    cuisine = CharField(model_attr='cuisine')
     pub_date = DateTimeField(model_attr='pub_date')
-
     def get_queryset(self):
         """Used when the entire index for the recipe model is updated"""
-        return Recipe.objects.filter(pub_date__lte=datetime.datetime.now())
+        return Recipe.objects.filter(shared=Recipe.SHARE_SHARED, pub_date__lte=datetime.datetime.now()) #only index shared recipes no private
 
 site.register(Recipe, RecipeIndex)
