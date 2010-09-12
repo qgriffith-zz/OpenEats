9ad0819696065c2ecdbfc1ce77786fa627a92a01
Quenten Griffith <qgriffith@gmail.com>
2010-09-12 18:05:17 -0400

    added some very basic test scripts for recipe and ingreidents

---
+++ recipe/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from models import Recipe
+
+class RecipeTestCast(TestCase):
+    fixtures = ['recipe_data.json', 'recipe']  #load up a recipe so we can do stuff with it
+
+    def setUp(self):
+        self.recipe = Recipe.objects.get(pk=1)
+
+    def testRecipe(self):
+        self.assertEquals(self.recipe.title, 'Chili')
+        self.assertEquals(self.recipe.slug, 'chili')
+        self.recipe.servings = 10
+        self.recipe.save()
+        self.assertNotEqual(self.recipe.servings, 20)
+
+    def tearDown(self):
+        self.recipe.delete()
