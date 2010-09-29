284fd20aa04b59481f63e9e87bc6c381e6693ca1
Quenten Griffith <qgriffith@gmail.com>
2010-09-29 17:03:39 -0400

    changed the url for the user profile to use get_absoulte_url instead of hard coding it in the login menu, also changed the logout page to redirect you to the main page and display a message that you where logged out instead of having a seperate log out page

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.contrib import messages
+
+def logout_page(request):
+    logout(request)
+    messages.success(request, 'You have been logged out!.')
+    return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
