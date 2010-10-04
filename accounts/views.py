95e401aca9f5e4433bd1f5df7bc04d3b46603065
Quenten Griffith <qgriffith@gmail.com>
2010-10-04 19:32:45 -0400

    added a new sign in page that users jquery tabs to show sign and signup forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.conf import settings
 from django.contrib import messages
 
 def logout_page(request):
+    '''this replaces the normal logout view found in django.contrib.auth with a view that logs the user out and redirects them to the main page
+       and displays a message
+    '''
     logout(request)
     messages.success(request, 'You have been logged out!.')
     return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+
+def signIn_page(request):
+    '''This combines the sign in and the create user forms into one page using jquery UI tabs and is called from the nav bar'''
+    return render_to_response('accounts/signIn_page.html', context_instance=RequestContext(request))
