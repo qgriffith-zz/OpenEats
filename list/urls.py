71802ab4a3a07e31ff0129bef48fd5be72517e79
Quenten Griffith <qgriffith@gmail.com>
2010-11-16 08:15:25 -0500

    added a delete function to remove users grocery list

--- list/urls.py
+++ list/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^grocery/$', 'list.views.index', name="grocery_list"),
+    url(r'^grocery/delete/(?P<id>\d+)/$', 'list.views.groceryDelete', name='grocery_delete'),
 
    )
--- list/views.py
+++ list/views.py
@@ -1,6 +1,9 @@
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.urlresolvers import reverse
 from models import GroceryList
 
 @login_required
@@ -8,3 +11,11 @@ def index(request):
     '''returns a list of grocery list for a user'''
     glist = GroceryList.objects.filter(author=request.user)
     return render_to_response('list/grocery_index.html', {'glists' : glist}, context_instance=RequestContext(request))
+
+@login_required
+def groceryDelete(request, id):
+    ''' takes the id of a list andremoves a users grocery list'''
+    list = get_object_or_404(GroceryList, author=request.user, id=id)
+    list.delete()
+    messages.success(request, 'Your grocery list has been removed.')
+    return HttpResponseRedirect(reverse('list.views.index'))
