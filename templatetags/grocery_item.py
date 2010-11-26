7c76185e271f17f5f66407b112cd8080e74ec5f8
Quenten Griffith <qgriffith@gmail.com>
2010-11-26 15:24:15 -0500

    added some style to the grocery list

--- templatetags/grocery_item.py
+++ templatetags/grocery_item.py
@@ -8,6 +8,6 @@ def item_tag(items):
     for item in items:
         if item.aisle not in aisle_list: #go thorugh and add each aisle to a list and only add each one once
             aisle_list.append(item.aisle)
-
+    
     aisle_list.sort()
-    return {'aisle_list':aisle_list, 'items':items }
+    return {'aisle_list':aisle_list, 'items':items, 'dict': dict }
