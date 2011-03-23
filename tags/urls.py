454fda8eff7af7dcd9848112cf7a8a7011df251c
Quenten Griffith <qgriffith@gmail.com>
2011-03-23 18:40:05 -0400

    created a recipe tag view.  Now you can see a list of recipes with a giving tag by clicking on a tag on any recipe.

--- tags/urls.py
+++ tags/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^recipe/(?P<tag>[-\w]+)/$', 'tags.views.recipeTags', name="recpie_tags"),
+    url(r'^recipe/(?P<tag>[-\w]+)/$', 'tags.views.recipeTags', name="recipe_tags"),
    )
