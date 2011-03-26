ebf6a3c010dd72ce80959235dd6d24fdd2dba67e
Quenten Griffith <qgriffith@gmail.com>
2011-03-26 11:23:13 -0400

    changed the sidebar browse templatetag to only show cuisine and courses that have recipes assigned to them.

--- templatetags/sidebar.py
+++ templatetags/sidebar.py
@@ -3,9 +3,18 @@ from openeats.recipe_groups.models import Course,Cuisine
 from itertools import chain
 register = template.Library()
 
-@register.inclusion_tag('sidebar/browse.html', takes_context=True) #get the request context so the sidebar templates have acess to the session data
+@register.inclusion_tag('sidebar/browse.html', takes_context=True) #get the request context so the sidebar templates have access to the session data
 def browse_tag(context):
-    course_list = Course.objects.all()
-    cuisine_list = Cuisine.objects.all()
+    courses = Course.objects.all()
+    cuisines = Cuisine.objects.all()
+    course_list = []
+    cuisine_list = []
+
+    for course in courses:
+        if course.recipe_count() > 0:  #check to see there is at least one recipe
+            course_list.append(course)
+    for cuisine in cuisines:
+        if cuisine.recipe_count() > 0:
+            cuisine_list.append(cuisine)
     return {'course_list':course_list, 'cuisine_list':cuisine_list, "request":context['request']}
 
