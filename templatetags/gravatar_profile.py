53246f5fdee06ef4395187f6d4220ccff13b319a
Quenten Griffith <qgriffith@gmail.com>
2011-03-29 20:11:41 -0400

    changed the gravatar templetag to use a diffrent default picture by passing mm to the d= paramater for mm: (mystery-man) a simple, cartoon-style silhouetted outline of a person (does not vary by email hash)

--- templatetags/gravatar_profile.py
+++ templatetags/gravatar_profile.py
@@ -9,7 +9,8 @@ def gravatar(user, size=80):
     gravatar_url = "http://www.gravatar.com/avatar.php?"
     gravatar_url += urllib.urlencode({
         'gravatar_id':hashlib.md5(user.email).hexdigest(),
-        'size':str(size)})
+        'size':str(size),
+        'd':'mm'})
     return """<img src="%s" alt="gravatar for %s" />""" % (gravatar_url, user.username)
 
 register.simple_tag(gravatar)
