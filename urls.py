118bee0e1a5bd2013d49676990c0452fa9a1ce82
Quenten Griffith <qgriffith@gmail.com>
2011-04-11 18:03:54 -0400

    revereted back a file accidently removed the import to create users profiles on save when i was doing the code clean up

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from openeats.accounts.forms import ProfileForm
-
+import helpers.signals  #needed to import the signal for when a user is saved their profile is created
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
