70537e252fbfeafb92597a15d904bf21fd70d5f4
Quenten Griffith <qgriffith@gmail.com>
2011-04-17 15:11:46 -0400

    finished the django management command to export all the recipes from the command line into the meal master format

--- management/commands/export_recipes.py
+++ management/commands/export_recipes.py
@@ -1,17 +1,17 @@
 from django.core.management.base import NoArgsCommand, CommandError
 from openeats.recipe.models import Recipe
-from templatetags.meal_master_convert import convert_unit
+from django.template import Context, loader
 
 class Command(NoArgsCommand):
     help = 'Outputs OpenEat recipes in the meal master format'
 
     def handle(self, *args, **options):
-
         #get a list of all the recipes
         try:
             recipes = Recipe.objects.all()
         except IndexError:
             raise CommandError("Could not get a list of recipes check your database")
-        for recipe in recipes:
-            print recipe.title
-            
+        
+        template = loader.get_template('recipe/mealmaster_export.txt')
+        context = Context({'queryset':recipes})
+        print template.render(context)
