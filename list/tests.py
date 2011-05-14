dcdc85d89e7872cc4fcac6da23fbd419824e81fb
Quenten Griffith <qgriffith@gmail.com>
2011-05-14 16:04:35 -0400

    added a bit more to the grocery list test

--- list/tests.py
+++ list/tests.py
@@ -5,11 +5,21 @@ from list.models import GroceryList, GroceryItem, GroceryShared, GroceryAisle
 
 class listViewsTestCase(TestCase):
     fixtures=['user_data.json', 'list_test_data.json', 'aisle_data.json']
-    def test_index(self):
+    def setUp(self):
         self.client.login(username="testUser", password='password')
-        self.user = User.objects.get(pk=2)
+
+    def test_index(self):
+        '''test we get a list of grocery list for a giving user'''
         resp = self.client.get(reverse('grocery_list'))
         self.assertEqual(resp.status_code, 200)
-    
+        self.assertTrue('glists' in resp.context)
+        self.assertEqual([glist.pk for glist in resp.context['glists']], [1])
+
+    def test_detail(self):
+        '''test we get a list and all of its items'''
+        resp = self.client.get(reverse('grocery_show', kwargs={'user':'testUser', 'slug':'test'}))
+        self.assertEqual(resp.status_code, 200)
+        
+
 
 
