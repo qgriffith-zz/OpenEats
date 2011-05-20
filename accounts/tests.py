e836e6b66965651d12c2c7582c3acf62bab5d6ee
Quenten Griffith <qgriffith@gmail.com>
2011-05-20 06:24:28 -0400

    Using a new python library called webtest to perfom form test.  Created a test to test the login form

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,33 +1,24 @@
-from django.test import TestCase
+from django_webtest import WebTest
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
-from accounts.models import UserProfiles
 
-class AccountTestCase(TestCase):
-    fixtures = ['data_user.json',' auth_user']
-    def setUp(self):
-        self.newUser = User.objects.create(username='newUser')
-        self.newUser.set_password('password')
-        self.newUser.save()
-        self.assertEquals(self.newUser.username, 'newUser')
-        self.newProfile = UserProfiles.objects.create(user=self.newUser, location="TestVille")
-                
-    def testLogin(self):
-        '''Test that the login page works for a user that was loaded from a fixture'''
-        self.assertTrue(self.client.login(username='testUser', password='password'))
-        self.client.logout()
-       
-    def testProfile(self):
-        '''Test that creation of a profile and edit the profile'''
-        self.login = self.client.login(username='newUser', password='password')
-        self.assertTrue(self.login)
-        response = self.client.get('/profiles/newUser/')
-        self.failUnlessEqual(response.status_code, 200)
-        self.newProfile.location = "MyVille"
-        self.newProfile.save()
-        self.assertEquals(self.newProfile.location, 'MyVille')
-           
-    def tearDown(self):
-       self.client.logout()
-       User.objects.all().delete()
-       UserProfiles.objects.all().delete()
+class accountViewsTestCase(WebTest):
+    fixtures = ['test_user_data.json']
+    setup_auth = False
+    def test_login(self):
+        '''used to test the login form'''
+
+        #sanity check to make sure the data loaded
+        user = User.objects.get(username="testUser")
+        self.assertEqual(user.username, 'testUser')
+        self.assertTrue(user.check_password('password'))
+
+        #login to the form
+        form = self.app.get(reverse('auth_login')).forms[1]
+        form['username'] = 'testUser'
+        form['password'] = 'password'
+        resp = form.submit()
+        self.assertEqual(resp.status, '302 FOUND')
+        self.assertEqual(resp.location, 'http://localhost:80' + reverse('recipe_index'))
         
+
