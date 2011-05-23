b4b909d6dd4888f340ea70ba92b27c21adfeaa32
Quenten Griffith <qgriffith@gmail.com>
2011-05-23 08:54:08 -0400

    Bug fix for adding courses and cuisine via admin page

--- recipe_groups/admin.py
+++ recipe_groups/admin.py
@@ -5,13 +5,13 @@ class CourseAdmin(admin.ModelAdmin):
     ordering = ['title']
     list_display = ['title', 'author']
     list_filter = ['author',]
-    prepopulated_fields = { 'slug' : ['title']}
+    
 
 class CuisineAdmin(admin.ModelAdmin):
     ordering = ['title']
     list_display = ['title', 'author']
     list_filter = ['author',]
-    prepopulated_fields = { 'slug' : ['title']}
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Cuisine, CuisineAdmin)
