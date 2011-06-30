80beff94c9e831f2a28588765ab7005687432209
Quenten Griffith <qgriffith@gmail.com>
2011-06-30 18:43:07 -0400

    changed the context processor for auth to the new location for django 1.3 breakkng django 1.2 compatiablity

--- settings.py
+++ settings.py
@@ -86,7 +86,7 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.i18n",
     "django.core.context_processors.debug",
     "django.core.context_processors.media",   
