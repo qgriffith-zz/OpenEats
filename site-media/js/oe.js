aa414950104b7cbc961fa42cd42cb969040e7403
Quenten Griffith <qgriffith@gmail.com>
2011-06-30 18:39:58 -0400

    added csfr protection to all ajax request

--- site-media/js/oe.js
+++ site-media/js/oe.js
@@ -1,30 +1,42 @@
 /* javascript file for OpenEats2*/
 
-/* this function is for the django-dynamic-formset which allows a new ingredient row to be added on the recipe form*/
-$(function() {
-    $('tbody tr').formset({
-        prefix: '{{ formset.prefix }}',
-        added: function(row){
-            var txt = row.find('#ing-field input');
-            txt.unbind();
-            makeAutoComplete(); /* call the makeAutoComplete function to assign the jquery ui autocomplete to the new row added*/
-        },
-        extraClasses: ['row1', 'row2']
+/* csfr send*/
+$(document).ajaxSend(function(event, xhr, settings) {
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    function sameOrigin(url) {
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var sr_origin = '//' + host;
+        var origin = protocol + sr_origin;
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+    }
+    function safeMethod(method) {
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
 
-    });
-})
-
-
-function makeAutoComplete() {
-    $(function() {
-        $("#ing-field input").autocomplete({
-            source: '/ingredient/auto/',
-            minLength: 2
-        });
-    })
-    };
-makeAutoComplete()
-
+    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    }
+});
 
 
 
