bb55997803d5405622a477fb403a7b8cf1d8ce12
Quenten Griffith <qgriffith@gmail.com>
2011-07-02 16:17:13 -0400

    renamed static to static-files

--- settings.py
+++ settings.py
@@ -62,13 +62,13 @@ USE_L10N = True
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = os.path.join(BASE_PATH, 'site-media')
-STATIC_ROOT = os.path.join(BASE_PATH, 'static')
+STATIC_ROOT = os.path.join(BASE_PATH, 'static-files')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = '/site-media/'
-STATIC_URL = '/static/'
+STATIC_URL = '/static-files/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
