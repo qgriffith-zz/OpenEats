0a7ea4579817be650a27ae9335123a249e879b8f
Quenten Griffith <qgriffith@gmail.com>
2011-09-03 14:21:00 -0400

    moved the api to the contrib folder since it is not going to be used by everyone I am making it an extra package instead of part of the main code

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from tastypie.api import Api
-from api import RecipeResource
-
-recipe_resource = RecipeResource()
-
-urlpatterns = patterns('',
-    (r'^v1/', include(recipe_resource.urls)),
-)
