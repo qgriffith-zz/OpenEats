fd5358d9bcb9851206a292dddde5ca10d5626577
Quenten Griffith <qgriffith@gmail.com>
2012-01-16 11:35:23 -0500

    Start of work for issue #4 to send recipes via email.  This is the form and the url to send the recipe.

--- recipe/forms.py
+++ recipe/forms.py
@@ -4,6 +4,12 @@ from models import Recipe
 from recipe_groups.models import Course, Cuisine
 import django.forms as forms
 from django.forms.models import BaseInlineFormSet
+from django.core.mail import EmailMessage, BadHeaderError
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.template import loader, RequestContext
+from django.http import HttpResponse
 
 class SelectWithPop(forms.Select):
     '''
@@ -41,3 +47,53 @@ class IngItemFormSet(BaseInlineFormSet):
          if completed < 2:
              raise forms.ValidationError("At least two %s are required." %
                 self.model._meta.object_name.lower())
+
+class RecipeSendMail(forms.Form):
+    '''Recipe form to send a recipe via email'''
+    def __init__(self, data=None, files=None, request=None, *args, **kwargs):
+        if request is None:
+            raise TypeError("Keyword argument 'request must be supplies'")
+        super(RecipeSendMail, self).__init__(data=data, files=files, *args, **kwargs)
+        self.request = request
+        #set up the return email address and sender name to the user logged in
+        if request.user.is_authenticated():
+            self.fields['to_email'].initial= request.user.email
+
+
+    to_email = forms.EmailField(widget=forms.TextInput(),label=_('email address'))
+    id = forms.CharField(widget=forms.HiddenInput())
+
+    from_email = settings.DEFAULT_FROM_EMAIL
+    from_site = Site.objects.get_current()
+    subject = _('Recipe from ' + str(from_site))
+
+
+    def get_body(self):
+        '''get the recipe and return the message body for the email'''
+        if self.is_valid():
+            recipe = Recipe.objects.get(pk = self.cleaned_data['id'])
+            template_name = 'recipe/recipe_mail_body.html' #template that contains the email body and also shared by the grocery print view
+            message = loader.render_to_string(template_name, {'recipe': recipe}, context_instance=RequestContext(self.request))
+            return message
+        else:
+            raise ValueError(_('Can not get the recipe id from invalid form data'))
+
+    def get_toMail(self):
+        '''gets the email to send the list to from the form'''
+        if self.is_valid():
+            return self.cleaned_data['to_email']
+        else:
+            raise ValueError(_('Can not get to_email from invalid form data'))
+
+    def save(self, fail_silently=False):
+        ''' sends the email message'''
+        if self.subject and self.get_body() and self.from_email:
+            try:
+                msg = EmailMessage(self.subject, self.get_body(), self.from_email, [self.get_toMail()])
+                msg.content_subtype = 'html'
+                msg.send()
+            except BadHeaderError:
+                return HttpResponse(_('Invalid header found.'))
+            return HttpResponse(_('Email Sent'))
+        else:
+         return HttpResponse('Make sure all fields are entered and valid.')
