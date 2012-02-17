ed0435ac42bf8ffefd1706303ca35f036776aee3
Quenten Griffith <qgriffith@gmail.com>
2012-02-17 10:44:13 -0500

    added the template and view, to view a full news entry

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
    # url(r'^list/$', 'news.views.list', name="news_list"),
-    #url(r'^entry/(?P<title>[\w-]+)/$', 'news.views.entry', name="news_entry"),
+   url(r'^entry/(?P<slug>[\w-]+)/$', 'news.views.entry', name="news_entry"),
 
    )
--- news/views.py
+++ news/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
+from django.template import RequestContext
+from models import Entry
+
+def entry(request, slug):
+    entry = get_object_or_404(Entry, slug=slug)
+    return render_to_response('news/entry.html', {'entry':entry}, context_instance=RequestContext(request))
+
+
