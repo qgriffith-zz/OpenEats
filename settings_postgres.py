9ec83d626842e8c6e92e72b9ad95eacc84ea5778
qgriffith <qgriffith@gmail.com>
2015-02-18 11:21:08 -0500

    more 1.5 fixes and fixes the note on recipe java script

--- settings_postgres.py
+++ settings_postgres.py
@@ -177,19 +177,6 @@ DEBUG_TOOLBAR_CONFIG = {
 'INTERCEPT_REDIRECTS': False,
 }
 
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
-
-
 #Email Server Settings
 DEFAULT_FROM_EMAIL = ''
 EMAIL_HOST = ''
