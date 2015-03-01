747720f75ef4018498a21bb75db8678f91a6e414
qgriffith <qgriffith@gmail.com>
2015-03-01 10:37:08 -0500

    updates fancybox and more bootstrap 3 fixes

--- templatetags/recipe_links.py
+++ templatetags/recipe_links.py
@@ -7,29 +7,23 @@ register = template.Library()
 def fav_link(user, recipe_id):
     cur_language = translation.get_language()
     translation.activate(cur_language)
-    if  user.is_authenticated(): #make sure the user is signed in
-        check = StoredRecipe.objects.filter(user = user.id, recipe = recipe_id) #check to see if the recipe is stored
+    if user.is_authenticated():  # make sure the user is signed in
+        check = StoredRecipe.objects.filter(user=user.id, recipe=recipe_id)  # check to see if the recipe is stored
         if check:
-            return "<a class=\"btn-inverse\" href=\"#\">%s</a>" % translation.ugettext('bookmakred')
-        else: #must not be stored yet
-            return "<a class=\"btn-info\" id=\"recipe-store\">%s</a>" % translation.ugettext('favorite')
-            
+            return "<a class=\"btn btn-success btn-sm\" href=\"#\">%s</a>" % translation.ugettext('bookmakred')
+        else:  # must not be stored yet
+            return "<a class=\"btn btn-primary btn-sm\" id=\"recipe-store\">%s</a>" % translation.ugettext('favorite')
     else:
         recipe = Recipe.objects.get(pk=recipe_id)
-        return "<a href=\"/accounts/login?next=/recipe/%s/\">%s</a>" % (recipe.slug, translation.ugettext('favorite'))
+        return "<a class=\"btn btn-primary btn-sm\" href=\"/accounts/login?next=/recipe/%s/\">%s</a>" % (recipe.slug, translation.ugettext('favorite'))
 
 @register.simple_tag
 def report_link(user, recipe_id):
     cur_language = translation.get_language()
     translation.activate(cur_language)
-    if  user.is_authenticated(): #make sure the user is signed in
-        check = ReportedRecipe.objects.filter(recipe = recipe_id) #check to see if the recipe is stored
+    if user.is_authenticated():  # make sure the user is signed in
+        check = ReportedRecipe.objects.filter(recipe=recipe_id)  # check to see if the recipe is reported
         if check:
-            return "<a class=\"btn-danger disabled\" id=\"recipe-report\" href=\"#\" title=\"recipe has been reported as spam\">%s</a>" % translation.ugettext('reported!')
-        else: #must not been reported yet yet
-            return "<a class=\"btn-info\" id=\"recipe-report\" title=\"report inappropriate recipe to the admins\">%s</a>" % translation.ugettext('report')
-
-    else:
-        recipe = Recipe.objects.get(pk=recipe_id)
-        return "<a href=\"/accounts/login?next=/recipe/%s/\">%s</a>" % (recipe.slug, translation.ugettext('favorite'))
-    
+            return "<a class=\"btn btn-danger btn-sm disabled\" id=\"recipe-report\" href=\"#\" title=\"recipe has been reported as spam\">%s</a>" % translation.ugettext('reported!')
+        else:  # must not been reported yet yet
+            return "<a class=\"btn btn-primary btn-sm\" id=\"recipe-report\" title=\"report inappropriate recipe to the admins\">%s</a>" % translation.ugettext('report')
