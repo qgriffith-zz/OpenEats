0a24b216d0f8ab601f9c1e2a10ddadc3e8949c0d
Charles Lehner <cel@celehner.com>
2015-09-17 00:17:30 -0400

    Add feed for recipes by tag

--- feed/urls.py
+++ feed/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import *
-from views import RecentRecipesFeed,TopRecipesFeed
+from views import RecentRecipesFeed,TopRecipesFeed,TaggedRecipesFeed
 
 urlpatterns = patterns('',
     (r'^recent/$', RecentRecipesFeed()),
     (r'^top/$', TopRecipesFeed()),
+    (r'^tag/(?P<tag>[-\w]+)/$', TaggedRecipesFeed()),
 )
