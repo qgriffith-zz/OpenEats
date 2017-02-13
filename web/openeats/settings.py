571a7fa00d16ced4c4b70fe025074261f69cca0a
Pando85 <pando855@gmail.com>
2017-02-13 21:28:54 +0100

    [Add] Smtp variables in production

--- web/openeats/settings.py
+++ web/openeats/settings.py
@@ -175,7 +175,9 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_PORT = os.environ.get('EMAIL_PORT', '')
-#EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+if os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true':
+    EMAIL_USE_TLS = True
 
 #registration
 LOGIN_REDIRECT_URL = "/recipe/"
